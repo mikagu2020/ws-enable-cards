c6bdf1c951f962ff342546d47d28a7655aec0809
mikagu <mikagu2020@gmail.com>
2020-06-03 13:21:18 +0200

    Implementado un Hash Map para recoger los datos y los correspondientes TextView para presentarlos.

--- app/src/main/java/com/example/infocard/MainActivity.java
+++ app/src/main/java/com/example/infocard/MainActivity.java
@@ -12,20 +12,26 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvInfocards, tvResponseHeaders;
+    TextView tvLastActiveEur, tvLastActiveInt, tvCardEurope, tvCardInternational, tvCurrentCard;
     Button btnActEu, btnActInt, btnInfoCards;
-    int statusCode;
+    String statusCode;
+    Map<String, String> records;
+    final static String LAST_ACTIVE_EUR = "lastactive_eur";
+    final static String LAST_ACTIVE_INT = "lastactive_int";
+    final static String CARD_EUROPE = "card_europe";
+    final static String CARD_INTERNATIONAL = "card_international";
+    final static String CURRENT_CARD = "current_card";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +42,17 @@ public class MainActivity extends AppCompatActivity {
 
         setEventListeners();
 
+        records = new HashMap<String, String>();
+
     }
 
     private void getControlViews() {
-        tvInfocards = findViewById(R.id.respose_text_field);
-        tvResponseHeaders = findViewById(R.id.respose_headers);
+        tvLastActiveEur = findViewById(R.id.last_active_eur);
+        tvLastActiveInt = findViewById(R.id.last_active_int);
+        tvCardEurope = findViewById(R.id.card_europe);
+        tvCardInternational = findViewById(R.id.card_international);
+        tvCurrentCard = findViewById(R.id.current_card);
+
         btnActEu = findViewById(R.id.btnActEu);
         btnActInt = findViewById(R.id.btnActInt);
         btnInfoCards = findViewById(R.id.btnInfoCards);
@@ -74,32 +86,45 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         parseToString(response);
+                        loadInfo();
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        statusCode = error.networkResponse.statusCode;
-                        tvResponseHeaders.setText(statusCode);
                         Toast.makeText(MainActivity.this,  error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-                });
+                }) {
+                @Override
+                    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+                        Map<String, String> headers = response.headers;
+                        statusCode = headers.get("X-Android-Response-Source");
+                        return super.parseNetworkResponse(response);
+                    }
+                };
         requestQueue.add(jsonObjectRequest);
     }
 
     private void parseToString(JSONObject response) {
         StringBuilder infoCards = new StringBuilder();
         JSONArray jsonArray = response.optJSONArray("records");
+        int n = jsonArray.length();
         try {
-            for (int i = 0, n = jsonArray.length(); i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 String key = jsonObject.keys().next();
                 String value = jsonObject.optString(key);
-                infoCards.append(key + ": ")
-                        .append(value + "\n");
+                records.put(key, value);
             }
-            tvInfocards.setText(infoCards.toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    private void loadInfo() {
+        tvLastActiveEur.setText("Última activacion europea: \n" + records.get(LAST_ACTIVE_EUR));
+        tvLastActiveInt.setText("Última activacion internacional: \n" + records.get(LAST_ACTIVE_INT));
+        tvCardEurope.setText("Tarjeta europea: \n" + records.get(CARD_EUROPE));
+        tvCardInternational.setText("Tarjeta internacional: \n" + records.get(CARD_INTERNATIONAL));
+        tvCurrentCard.setText("Última tarjeta activada: \n" + records.get(CURRENT_CARD));
     }
 }
